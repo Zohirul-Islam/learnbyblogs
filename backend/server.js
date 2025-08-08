d6982816b0930d436485b8a02c18dca0350e8693
bijoy <bijoycse24@gmail.com>
2025-08-08 13:48:44 +0600

    basic nodejs server setup

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import cors from 'cors';
+import 'dotenv/config'
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.get('/',(req,res)=>{res.send('Api is working')})
+
+const PORT = process.env.PORT ||3000;
+
+app.listen(PORT,()=>{
+    console.log("Server is runnning on port " + PORT);
+})
+
+export default app
