3aabb3c74e24a71097e7884a4e5bacf20bda709d
bijoy <bijoycse24@gmail.com>
2025-08-11 12:29:21 +0600

    fix something

--- backend/controllers/blogController.js
+++ backend/controllers/blogController.js
@@ -1,6 +1,7 @@
 import fs from 'fs';
 import imagekit from '../configs/imageKit.js';
 import Blog from '../models/blogModel.js';
+import Comment from '../models/commentModel.js';
 export const addBlog =async(req,res)=>{
         try {
             const {title,subTitle,description,category,isPublished} = JSON.parse(req.body.blog);
@@ -42,7 +43,8 @@ export const getAllBlog = async(req,res) =>{
 }
 export const getBlogById = async(req,res) =>{
     try {
-        const {blogId} = req.parse
+
+        const {blogId} = req.params
         const blog = await Blog.findById(blogId);
         if(!blog){
           return  res.json({success:false,message:"blog not found"})
@@ -59,6 +61,36 @@ export const deleteBlogById = async(req,res) =>{
         const blog = await Blog.findByIdAndDelete(id);
         res.json({success:true,message:"delete blog successfully"})
 
+    } catch (error) {
+            res.json({success:false,message:error.message})
+    }
+}
+export const togglePublish = async(req,res) =>{
+    try {
+        const {id} = req.body;
+        const blog = await Blog.findById(id);
+        blog.isPublished = !blog.isPublished;
+        await blog.save()
+        res.json({success:true,message:"blog status updated"})
+    } catch (error) {
+            res.json({success:false,message:error.message})
+    }
+}
+export const addComment = async(req,res) =>{
+    try {
+        const {blog,name,content}=req.body;
+        await Comment.create({blog,name,content})
+        res.json({success:true,message:"comment added to reviews"})
+    } catch (error) {
+            res.json({success:false,message:error.message})
+    }
+}
+export const getblogComment = async(req,res) =>{
+    try {
+        const {blogId} = req.body;
+        const comments = await Comment.find({blog:blogId,isApproved:true
+        }).sort({createdAt: -1})
+        res.json({success:true,comments})
     } catch (error) {
             res.json({success:false,message:error.message})
     }
